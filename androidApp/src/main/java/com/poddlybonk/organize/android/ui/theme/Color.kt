c55ad40b609dca88405a670545b6dbd92129f52f
Simon Rofe <simon.rofe@ieee.org>
2022-10-26 13:06:47 +1000

    Add android UI themeing to starter project

---
+++ androidApp/src/main/java/com/poddlybonk/organize/android/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.poddlybonk.organize.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorPrimary = Color(0xFF006837)
+val ColorPrimaryDark = Color(0xFF004012)
+val ColorAccent = Color(0xFFC75F00)
