35fcdd2ea0018f9059ee17cccabff5cfb3a93278
Simon Rofe <simon.rofe@ieee.org>
2022-10-26 13:59:24 +1000

    Update shared build scripts for starter project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 rootProject.name = "Organize"
 include(":androidApp")
+include(":desktopApp")
 include(":shared")
