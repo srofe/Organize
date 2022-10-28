4ff15b9b4cdf9037846810821922078a08ac2bbc
Simon Rofe <simon.rofe@ieee.org>
2022-10-28 15:47:08 +1000

    Updates to desktop RemindersView for starter project

--- desktopApp/src/jvmMain/kotlin/Main.kt
+++ desktopApp/src/jvmMain/kotlin/Main.kt
@@ -24,7 +24,7 @@ fun main() {
                 onCloseRequest = ::exitApplication
             ) {
                 RemindersView(
-                    onAboutIconClick = { screenState = Screen.AboutDevice }
+                    onAboutButtonClick = { screenState = Screen.AboutDevice }
                 )
             }
 
