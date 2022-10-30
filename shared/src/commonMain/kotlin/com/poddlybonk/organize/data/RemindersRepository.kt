67a49beaabd56402ae7434d43e602d27fc6e097a
Simon Rofe <simon.rofe@ieee.org>
2022-10-30 15:21:54 +1000

    Wrap repository inside its own class

--- shared/src/commonMain/kotlin/com/poddlybonk/organize/data/RemindersRepository.kt
+++ shared/src/commonMain/kotlin/com/poddlybonk/organize/data/RemindersRepository.kt
@@ -3,22 +3,24 @@ package com.poddlybonk.organize.data
 import com.poddlybonk.organize.domain.Reminder
 import com.poddlybonk.organize.domain.UUID
 
-private val _reminders: MutableList<Reminder> = mutableListOf()
-fun createReminder(title: String) {
-    val newReminder = Reminder(
-        id = UUID().toString(),
-        title = title,
-        isCompleted = false
-    )
-    _reminders.add(newReminder)
-}
+class RemindersRepository {
+    private val _reminders: MutableList<Reminder> = mutableListOf()
+    val reminders: List<Reminder>
+        get() = _reminders
 
-fun markReminder(id: String, isCompleted: Boolean) {
-    val index = _reminders.indexOfFirst { it.id == id }
-    if (index != -1) {
-        _reminders[index] = _reminders[index].copy(isCompleted = isCompleted)
+    fun createReminder(title: String) {
+        val newReminder = Reminder(
+            id = UUID().toString(),
+            title = title,
+            isCompleted = false
+        )
+        _reminders.add(newReminder)
+    }
+
+    fun markReminder(id: String, isCompleted: Boolean) {
+        val index = _reminders.indexOfFirst { it.id == id }
+        if (index != -1) {
+            _reminders[index] = _reminders[index].copy(isCompleted = isCompleted)
+        }
     }
 }
-
-val reminders: List<Reminder>
-    get() = _reminders
