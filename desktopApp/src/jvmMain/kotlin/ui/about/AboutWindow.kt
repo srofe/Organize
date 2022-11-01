7efd4b2de8e92887be012206ecd331368e9e9d9a
Simon Rofe <simon.rofe@ieee.org>
2022-11-02 09:02:19 +1000

    Remove unused import

--- desktopApp/src/jvmMain/kotlin/ui/about/AboutWindow.kt
+++ desktopApp/src/jvmMain/kotlin/ui/about/AboutWindow.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.rememberWindowState
 import com.poddlybonk.organize.presentation.AboutViewModel
 
 @Composable
