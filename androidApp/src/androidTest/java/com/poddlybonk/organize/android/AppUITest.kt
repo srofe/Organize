75dff7b06df016024679b1efbd50dbde55e34309
Simon Rofe <simon.rofe@ieee.org>
2022-11-08 06:02:44 +1000

    Add test for opening and closing about page

--- androidApp/src/androidTest/java/com/poddlybonk/organize/android/AppUITest.kt
+++ androidApp/src/androidTest/java/com/poddlybonk/organize/android/AppUITest.kt
@@ -3,6 +3,8 @@ package com.poddlybonk.organize.android
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.poddlybonk.organize.android.ui.root.MainActivity
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +17,22 @@ class AppUITest {
     fun testAboutButtonExistence() {
         composeTestRule
             .onNodeWithContentDescription("aboutButton")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun testOpeningAndClosingAboutPage() {
+        composeTestRule
+            .onNodeWithContentDescription("aboutButton")
+            .performClick()
+        composeTestRule
+            .onNodeWithText("About Device")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithContentDescription("Up Button")
+            .performClick()
+        composeTestRule
+            .onNodeWithText("Reminders")
             .assertIsDisplayed()
     }
 }
